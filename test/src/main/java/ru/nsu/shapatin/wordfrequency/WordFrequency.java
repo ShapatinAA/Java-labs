49ac6ce265fb8f6f57deeef621cf7ee2a05267cc
ShapatinAA <113165107+ShapatinAA@users.noreply.github.com>
2023-05-22 02:50:10 +0700

    Delete test directory

--- test/src/main/java/ru/nsu/shapatin/wordfrequency/WordFrequency.java
+++
@@ -1,9 +0,0 @@
-package ru.nsu.shapatin.wordfrequency;
-
-public record WordFrequency(String word, int frequency) implements Comparable<WordFrequency> {
-
-    @Override
-    public int compareTo(WordFrequency other) {
-        return Integer.compare(this.frequency, other.frequency);
-    }
-}
