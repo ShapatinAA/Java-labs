91a0fe006ce2871db88f8d26eb99f73d5bb91e91
ShapatinAA <113165107+ShapatinAA@users.noreply.github.com>
2023-05-22 00:13:01 +0700

    Add files via upload

---
+++ test/src/main/java/ru/nsu/shapatin/csvwriter/CSVWriter.java
@@ -0,0 +1,35 @@
+package ru.nsu.shapatin.csvwriter;
+
+import ru.nsu.shapatin.wordfrequency.WordFrequency;
+
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class CSVWriter {
+    public void writeToCSV(List<WordFrequency> wordFrequencyList) throws IOException {
+        String projectPath = new File("").getAbsolutePath();
+        String resourcesPath = Paths.get(projectPath, "src", "main", "resources").toString();
+        String csvFilePath = Paths.get(resourcesPath, "word_frequencies.csv").toString();
+
+        try (FileWriter writer = new FileWriter(csvFilePath)) {
+            StringBuilder csvContent = new StringBuilder("Word,Frequency,Frequency (%)\n");
+
+            int totalWords = wordFrequencyList.stream()
+                    .mapToInt(WordFrequency::frequency)
+                    .sum();
+
+            for (WordFrequency wordFrequency : wordFrequencyList) {
+                double frequencyPercentage = (double) wordFrequency.frequency() * 100 / totalWords;
+                csvContent.append(wordFrequency.word())
+                        .append(",")
+                        .append(wordFrequency.frequency())
+                        .append(",")
+                        .append(String.format("%.2f", frequencyPercentage))
+                        .append("%\n");
+            }
+
+            writer.write(csvContent.toString());
+        }
+    }
+}
